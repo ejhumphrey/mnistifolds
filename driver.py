67b02a68be00136d313a74877b57a9770829f94e
Eric J. Humphrey <ejhumphrey@nyu.edu>
2016-03-10 09:39:46 -0500

    Updated driver with a yaml config.

--- driver.py
+++ driver.py
@@ -1,68 +1,57 @@
-"""Demonstration of building an MNIST classifier.
+"""Demonstration of building an MNIST embedding.
 
-
-This script and its corresponding functionality expects the gzipped pickle file
-provided courtesy of the Theano dev team at Montreal:
-
-    http://deeplearning.net/data/mnist/mnist.pkl.gz
-
-
-Sample call:
-python ./examples/mnist.py \
-~/mnist.pkl.gz \
-./examples/sample_definition.json \
-examples/sample_params.npz
+Sample call
+-----------
+python ./driver.py \
+~/mnist.npz \
+model_definition.json \
+param_cache.hdf5 \
+--neighbor_graph.json
 """
 
 import argparse
-import numpy as np
+import biggie
+import json
 import optimus
-import datatools
+import os
+import pescador
+import yaml
 
+import datatools
 import models
 
 
-def main(args):
+def main(dataset, param_cache, model_name, hyperparams, train_params,
+         output_dir, neighbor_graph, verbose=False):
+
     # Create the models, and pass the trainer to a driver.
-    trainer, predictor = models.pwrank()
-    driver = optimus.Driver(graph=trainer)
+    trainer, predictor = models.pwrank(verbose)
 
     # Load data and configure the minibatch generator.
-    train, valid, test = datatools.load_mnist(args.mnist_file)
-    stream = datatools.minibatch(train[0], train[1], 50)
-    hyperparams = dict(learning_rate=0.02)
+    train, valid, test = datatools.load_mnist_npz(os.path.expanduser(dataset))
+    nn_idxs = json.load(open(os.path.expanduser(neighbor_graph)))
+    cstream = datatools.neighbor_stream(train[0], nn_idxs)
+    stream = pescador.buffer_batch(cstream, hyperparams.pop("batch_size"))
 
     # And we're off!
-    stats = driver.fit(stream, hyperparams=hyperparams, max_iter=500,
-                       print_freq=20)
-
+    output_dir = os.path.expanduser(output_dir)
+    params = biggie.Stash(os.path.join(output_dir, param_cache))
+    driver = optimus.Driver(name=model_name, graph=trainer,
+                            parameter_cache=params)
+    stats = driver.fit(stream, hyperparams=hyperparams, **train_params)
     print(stats)
-    # Note that, because they were made at the same time, `trainer` and
-    #   `predictor` share the same parameters.
-    y_pred = predictor(valid[0])['likelihoods'].argmax(axis=1)
-    print("Accuracy: {0:0.4}".format(np.equal(valid[1], y_pred).mean()))
 
-    # Now that it's done, save the graph and parameters to disk.
-    optimus.save(predictor, args.def_file, args.param_file)
-
-    # And, because we can, let's recreate it purely from disk.
-    predictor2 = optimus.load(args.def_file, args.param_file)
-    y_pred2 = predictor2(valid[0])['likelihoods'].argmax(axis=1)
-    print("Consistency: {0:0.4}".format(np.equal(y_pred, y_pred2).mean()))
+    optimus.save(
+        predictor, os.path.join(output_dir, "{}.json".format(model_name)))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
 
     # Inputs
-    parser.add_argument("mnist_file",
-                        metavar="mnist_file", type=str,
-                        help="Path to the MNIST data file.")
-    # Outputs
-    parser.add_argument("def_file",
-                        metavar="def_file", type=str,
-                        help="Path to save the classifier graph.")
-    parser.add_argument("param_file",
-                        metavar="param_file", type=str,
-                        help="Path to save the classifier parameters.")
-    main(parser.parse_args())
+    parser.add_argument("config_file",
+                        metavar="config_file", type=str,
+                        help="Path to a yaml config file.")
+    args = parser.parse_args()
+    kwargs = yaml.load(open(args.config_file))
+    main(**kwargs)
